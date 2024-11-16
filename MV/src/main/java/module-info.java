4da40710e04479edebcb8292631647c0ff14f434
thRollingStones <129461036+thRollingStones@users.noreply.github.com>
2024-11-15 21:52:51 -0500

    Delete MV directory

--- MV/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.mv {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.json;
-
-    opens com.example.mv to javafx.fxml;
-    exports com.example.mv;
-}
