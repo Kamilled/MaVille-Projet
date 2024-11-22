c92b043cc9bfe72356f9c16addeb27a0da46e66c
Aya Elbroumi <145565486+ayaelb8@users.noreply.github.com>
2024-11-22 10:53:34 -0500

    test Utilisateur

--- prototype/src/main/java/com/example/prototype/UtilisateurTest.java
+++ prototype/src/main/java/com/example/prototype/UtilisateurTest.java
@@ -1,8 +1,41 @@
 package com.example.prototype;
-import org.testng.annotations.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 
 public class UtilisateurTest {
 
+    @Test
+    void testauthentifier() {
+        //test résident
+        Utilisateur utilisateur1 = new Utilisateur("1@hotmail.com", "123", "resident");
+        assertTrue(utilisateur1.authentifier("1@hotmail.com", "123"));
+        //test intervenant
+        Utilisateur utilisateur2 = new Utilisateur("1@gmail.com", "123", "intervenant");
+        assertTrue(utilisateur2.authentifier("1@gmail.com", "123"));
+    }
 
+    @Test
+    void testauthentifierMauvaisMDP() {
+        //test avec mot de passe incorrect
+        //test résident
+        Utilisateur utilisateur3 = new Utilisateur("1@hotmail.com", "123", "resident");
+        assertFalse(utilisateur3.authentifier("1@hotmail.com", "MauvaisMotDePasse"));
+        //test intervenant
+        Utilisateur utilisateur4 = new Utilisateur("1@gmail.com", "123", "intervenant");
+        assertFalse(utilisateur4.authentifier("1@gmail.com", "MauvaisMotDePasse"));
+    }
+
+    @Test
+    void testauthentifierMauvaisCourriel() {
+        //test avec email incorrect
+        //test résident
+        Utilisateur utilisateur5 = new Utilisateur("1@hotmail.com", "123", "resident");
+        assertFalse(utilisateur5.authentifier("mauvaisCourriel", "123"));
+        //test intervenant
+        Utilisateur utilisateur6 = new Utilisateur("1@gmail.com", "123", "intervenant");
+        assertFalse(utilisateur6.authentifier("mauvaisCourriel", "123"));
+    }
 }
